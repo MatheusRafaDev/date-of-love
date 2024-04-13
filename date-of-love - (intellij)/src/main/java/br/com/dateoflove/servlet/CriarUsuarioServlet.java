6124b85139725ac2413e413f1ff06fa35dcee5e3
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2024-04-13 19:03:06 -0300

    subindo css e colocando telas de criar conta

--- date-of-love - (intellij)/src/main/java/br/com/dateoflove/servlet/CriarUsuarioServlet.java
+++ date-of-love - (intellij)/src/main/java/br/com/dateoflove/servlet/CriarUsuarioServlet.java
@@ -1,6 +1,5 @@
 package br.com.dateoflove.servlet;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,12 +13,26 @@ public class CriarUsuarioServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String usuarioNome = request.getParameter("usuario-nome");
+        String nomeNoivo = request.getParameter("nome_noivo");
+        String nomeNoiva = request.getParameter("nome_noiva");
+        String email = request.getParameter("email");
+        String dataCasamento = request.getParameter("data_casamento");
+        String estiloFesta = request.getParameter("estilo_festa");
+        String localizacao = request.getParameter("localizacao");
+        String numConvidados = request.getParameter("num_convidados");
+        String senha = request.getParameter("senha");
+        String confirmarSenha = request.getParameter("confirmar_senha");
 
-        System.out.println( usuarioNome);
+        System.out.println("Nome do Noivo: " + nomeNoivo);
+        System.out.println("Nome da Noiva: " + nomeNoiva);
+        System.out.println("Email: " + email);
+        System.out.println("Data do Casamento: " + dataCasamento);
+        System.out.println("Estilo da Festa: " + estiloFesta);
+        System.out.println("Localização: " + localizacao);
+        System.out.println("Número de Convidados: " + numConvidados);
+        System.out.println("Senha: " + senha);
+        System.out.println("Confirmar Senha: " + confirmarSenha);
 
         request.getRequestDispatcher("index.html").forward(request, response);
-
     }
 }
-
