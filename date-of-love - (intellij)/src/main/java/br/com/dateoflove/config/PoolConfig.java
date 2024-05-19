90dcd5e7be1a897c81a943989d39c5bda55441ad
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2024-05-19 19:07:50 -0300

    linkando com o banco azure

--- date-of-love - (intellij)/src/main/java/br/com/dateoflove/config/PoolConfig.java
+++ date-of-love - (intellij)/src/main/java/br/com/dateoflove/config/PoolConfig.java
@@ -1,7 +1,6 @@
 package br.com.dateoflove.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -17,26 +16,21 @@ public class PoolConfig {
 
         if (dataSource == null) {
             dataSource = new BasicDataSource();
-            dataSource.setUrl("date-of-love.mysql.database.azure.com");
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://date-of-love.mysql.database.azure.com:3306/dateoflove");
             dataSource.setUsername("dateoflove");
-            dataSource.setPassword("Matheus123@");
+            dataSource.setPassword("matheus123@");
             dataSource.setMinIdle(5);
             dataSource.setMaxIdle(10);
             dataSource.setMaxTotal(50);
 
             System.out.println("Novo pool de conexões criado com sucesso");
-
         }
 
         return dataSource;
     }
 
     public static Connection getConnection() throws SQLException {
-
         return getDataSource().getConnection();
-
     }
-
 }
-
-
